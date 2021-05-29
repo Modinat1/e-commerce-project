99c6050376648e96627e32740cf82774fabdb374
Modinat1 <61569657+Modinat1@users.noreply.github.com>
2021-05-29 09:51:17 +0100

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,13 @@
+const images = document.getElementById("images");
+const imgCounter = document.querySelectorAll("#images img");
+
+let index = 0;
+function carouselOpener(){
+    if(index < imgCounter.length-1){
+    index++;
+    }else{
+        index = 0;
+    }
+images.style.transform = `translateX(${-index * 550}px )`;
+}
+setInterval(carouselOpener, 2000);
